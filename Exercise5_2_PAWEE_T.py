9ef257c2953ddc715620acca7e4ad1bf6ae6a552
Jaaoeyey <th.pawee@gmail.com>
2021-10-11 10:36:30 +0700

    Create Exercise5_2_PAWEE_T.py

---
+++ Exercise5_2_PAWEE_T.py
@@ -0,0 +1,8 @@
+print("Please input time(hr)and distance(km) the value should be greater than 1")
+s = int(input("Please input distance(km): "))
+t = int(input("Please input time(hr): "))
+
+v = str(s/t)
+print("Speed: ", v, "km/hr")
+
+
